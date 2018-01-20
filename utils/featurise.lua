49b8129e1fdcee9d8fe8dbe1d524dc9813583d44
Petar Veličković <pv273@cam.ac.uk>
2018-01-20 15:13:34 +0000

    Featurisation script update

--- utils/featurise.lua
+++ utils/featurise.lua
@@ -1,7 +1,7 @@
 require 'audio'
 npy4th = require 'npy4th'
 
-sound = audio.load(arg[1]):mul(2^-23):view(1,1,-1,1)
+sound = audio.load(arg[1]):select(2,1):clone():mul(2^-23):view(1,1,-1,1)
 
 npy4th.savenpy(arg[2], sound)
 
