0fab3d72162b9b69be7c5a6866a4c17e30f12f3e
Petar Veličković <pv273@cam.ac.uk>
2018-01-21 01:10:32 +0000

    Bridge chatter

--- backend/bridge.py
+++ backend/bridge.py
@@ -21,31 +21,52 @@ class Bridge:
     # use_plc: whether to use places
     # use_chatter: whether to use chatter or normal mode
     # Returns: a vector of shape (10,) containing volume levels
-    def get_sound(self, obj_dist, plc_dist, chatter_level=None, use_obj=True, use_plc=True, use_chatter=False):
-        # compute the closest obj/plc out of 1024 possible choices.
-        # using kl distance
-
-        entropy_obj = entropy(self.objs, obj_dist)
-        entropy_plc = entropy(self.plcs, plc_dist)
-
-        assert entropy_obj.shape == (1024,)
-        assert entropy_plc.shape == (1024,)
-
-        total_entropy = np.zeros_like(entropy_obj)
-
-        if use_obj:
-            total_entropy += entropy_obj
-        if use_plc:
-            total_entropy += entropy_plc
-
-        minimum = np.nanargmin(total_entropy)
+    def get_sound(self, obj_dist, plc_dist, chatter_level=0.0, use_obj=True, use_plc=True, use_chatter=False):
         out = np.zeros(10)
 
-        for i in range(len(out)):
-            if minimum & (1 << i):
-                out[i] = 1.0
-            else:
-                out[i] = 0.0
+        if use_chatter:
+            entropies = np.zeros(10)
+            for i in range(10):
+                entropy_obj = entropy(self.objs[1 << i], obj_dist)
+                entropy_plc = entropy(self.plcs[1 << i], plc_dist)
+
+                if use_obj:
+                    entropies[i] += entropy_obj
+                if use_plc:
+                    entropies[i] += entropy_plc
+
+            indices = np.argsort(entropies)
+
+            # -4.6 = ln 0.01
+            l = -4.6 * (chatter_level - 1.0)
+
+            for i in range(10):
+                out[indices[i]] = exp(-l * i)
+
+        else:
+            # compute the closest obj/plc out of 1024 possible choices.
+            # using kl distance
+
+            entropy_obj = entropy(self.objs, obj_dist)
+            entropy_plc = entropy(self.plcs, plc_dist)
+
+            assert entropy_obj.shape == (1024,)
+            assert entropy_plc.shape == (1024,)
+
+            total_entropy = np.zeros_like(entropy_obj)
+
+            if use_obj:
+                total_entropy += entropy_obj
+            if use_plc:
+                total_entropy += entropy_plc
+
+            minimum = np.nanargmin(total_entropy)
+
+            for i in range(len(out)):
+                if minimum & (1 << i):
+                    out[i] = 1.0
+                else:
+                    out[i] = 0.0
 
         return out 
 
