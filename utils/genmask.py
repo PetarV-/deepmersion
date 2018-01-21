dc8373f4f6ad0bd44ea517938ae811ac9aebd99a
Petar Veličković <pv273@cam.ac.uk>
2018-01-21 11:11:56 +0000

    Fix mask!

--- utils/genmask.py
+++ utils/genmask.py
@@ -2,20 +2,24 @@ import numpy as np
 import sys
 
 mst = {}
+mst2 = {}
 
-indices = np.empty(365)
+indices = np.zeros(365)
 
 with open(sys.argv[2], 'r') as f:
     for line in f:
         els = line.split()
         mst[els[0]] = els[1]
 
+tot = 0
 with open(sys.argv[1], 'r') as f:
     for line in f:
         els = line.split()
         if els[0] in mst:
             indices[int(mst[els[0]])] = els[1]
             print(mst[els[0]], '->', els[1])
-    
+            tot += 1
+            print(tot)
+
 np.save('mask.npy', indices)
 
