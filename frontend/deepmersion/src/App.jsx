e6921a0926d8924525bd6bf45645aefa45d27a01
Matthew Else <matthewelse1997@gmail.com>
2018-01-21 01:05:30 +0000

    Abstract DropZone into separate component

--- frontend/deepmersion/src/App.jsx
+++ frontend/deepmersion/src/App.jsx
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 
-import { PageHeader, Grid, Row, Col, Label } from "react-bootstrap";
-
-import Dropzone from "react-dropzone";
-import request from "superagent";
-
-import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import faFileImage from "@fortawesome/fontawesome-free-regular/faFileImage";
+import { PageHeader } from "react-bootstrap";
 
 import { AudioGrid } from "./AudioChoices";
-import { TagList } from "./TagList";
 import { Controls } from "./Controls";
+import { DropZone } from "./DropZone";
+
 import "./App.css";
 import logo from "./logo.svg";
 
@@ -37,8 +32,6 @@ class App extends Component {
 
       masterVolume: 0.5,
       volumes: this.sounds.map(_ => 0),
-      objectTags: [],
-      placeTags: [],
 
       useObjects: true,
       useChatter: true,
@@ -47,54 +40,7 @@ class App extends Component {
   }
 
   randomizeSounds = () => {
-    let newVolumes = this.sounds.map(_ => {
-      let r = Math.random();
-      if (r < 0.5) {
-        return 0;
-      }
-
-      return r;
-    });
-
-    this.setState({ volumes: newVolumes, muted: false });
-  };
-
-  classify = image => {
-    request
-      .post("/classify")
-      .field("useObjects", this.state.useObjects)
-      .field("useChatter", this.state.useChatter)
-      .field("usePlaces", this.state.usePlaces)
-      .attach("image", image)
-      .then(res => {
-        let objectTags = res.body.objectTags;
-        let placeTags = res.body.placeTags;
-
-        let volumes = res.body.volumes;
-        console.log(volumes);
-
-        this.setState({
-          volumes: volumes,
-          muted: false,
-
-          objectTags: objectTags || [],
-          placeTags: placeTags || []
-        });
-      });
-  };
-
-  onDrop = (accepted, rejected) => {
-    // ask the server for an audio configuration
-    this.classify(accepted[0]);
-
-    // update the preview
-    let reader = new FileReader();
-
-    reader.onload = img => {
-      console.log("image loaded");
-      this.refs.preview.src = img.target.result;
-    };
-    reader.readAsDataURL(accepted[0]);
+    this.setState({ volumes: this.sounds.map(Math.random), muted: false });
   };
 
   toggle = key => {
@@ -102,6 +48,13 @@ class App extends Component {
       let result = {};
       result[key] = !previous[key];
       return result;
+    });
+  };
+
+  updateVolumes = volumes => {
+    this.setState({
+      volumes: volumes,
+      muted: false
     });
   };
 
@@ -133,33 +86,12 @@ class App extends Component {
           />
         </div>
 
-        <div className="container dragdrop">
-          <Grid>
-            <Row>
-              <Col md={4}>
-                <Dropzone
-                  accept="image/jpeg, image/png"
-                  multiple={false}
-                  onDrop={this.onDrop}
-                >
-                  <div className="dropzone">
-                    <FontAwesomeIcon icon={faFileImage} size="6x" />
-
-                    <p>Drop an image here.</p>
-                    <small>Accepts JPEG, PNG</small>
-                  </div>
-                </Dropzone>
-              </Col>
-              <Col md={4}>
-                <img ref="preview" className="previewImage" alt="" />
-              </Col>
-              <Col md={4}>
-                <TagList tags={this.state.objectTags} bsStyle="primary" />
-                <TagList tags={this.state.placeTags} bsStyle="success" />
-              </Col>
-            </Row>
-          </Grid>
-        </div>
+        <DropZone
+          useObjects={this.state.useObjects}
+          useChatter={this.state.useChatter}
+          usePlaces={this.state.usePlaces}
+          onClassify={this.updateVolumes}
+        />
 
         <AudioGrid
           ref="grid"
